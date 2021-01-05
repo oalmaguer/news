61099f773180ba0f5e5a6efdf15e26d342db31a8
oalmaguer <almaguero95@gmail.com>
2021-01-05 02:43:50 -0500

    new deploy

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import Card from 'react-bootstrap/Card';
+import Noimage from '../images/noimage.png';
 import {
   BrowserRouter as Router,
   Switch,
@@ -17,7 +18,7 @@ class Cards extends React.Component {
         articles: [],
         user: '',
         rememberMe: false,
-        language: '',
+        language: 'en',
         news: []
     };
 
@@ -65,7 +66,8 @@ class Cards extends React.Component {
   }
 
   handleLanguage(event) {
-      this.setState({language: event.target.value})
+      this.setState({language: event.target.value});
+      console.log(event.target.value);
   }
 
 //   handleSubmit(event) {
@@ -75,21 +77,65 @@ class Cards extends React.Component {
     
 
     getNews(event) {
-        const proxyUrl = "https://cors-anywhere.herokuapp.com/";
          var key = "23de237b3d5a45c2becdee407614db97";
-        axios.get(`${proxyUrl}https://newsapi.org/v2/everything?q=${this.state.topic}&language=${this.state.language}&apiKey=${key}&pageSize=50`)
+        // axios.get(`http://newsapi.org/v2/everything?q=${this.state.topic}&language=${this.state.language}&apiKey=${key}&pageSize=50`)
+        axios.get(`http://api.mediastack.com/v1/news?access_key=fbda70b804223eba9a77a64b26c47c2b&keywords=${this.state.topic}&languages=${this.state.language}&sort=published_desc`)
         .then(news => {
-            var articles = news.data.articles;
-            console.log(articles);
-                
+            var articles = news.data.data;
+                console.log(articles);
+                console.log(articles.length);
+
+            function removeDuplicates(originalArray, prop) {
+                var newArray = [];
+                var lookupObject  = {};
+
+                for(var i in originalArray) {
+                    lookupObject[originalArray[i][prop]] = originalArray[i];
+                }
+
+                for(i in lookupObject) {
+                    newArray.push(lookupObject[i]);
+                }
+                return newArray;
+            }
+
+            var uniqueArray = removeDuplicates(articles, "title");
+            console.log(uniqueArray);
+
+
+                // for (var i=0;i<50-1;i++) {
+                //     debugger;
+                //     if (articles[i].title == articles[i+1].title) {
+                //         articles.splice(i, 1);
+                //     }
+                // }
+
+
+            // articles.map((element, index) => {
+            //     debugger;
+            //     if (element[index].title == element[index+1].title ) {
+            //     console.log(index);
+            //         console.log(element);
+            //     } else {
+            //         return "hola";
+            //     }
+            // })
+            
+            
                 this.setState({
-                   articles: articles,
+                   articles: uniqueArray,
                 })
+                
             })
 
            event.preventDefault();
+
     }
     render() {
+        
+    
+        const maxlength = 250;
+        const image = '';
         return(
             
             <div>
@@ -122,25 +168,45 @@ class Cards extends React.Component {
 
                     </div>
                    
-
-                    <p></p>
                     </form>
                 </div>
 
                 </Container>
                     <Row className="justify-content-center">
                         {this.state.articles.map((article, index) => (
+
+                            
+                          
                             <Card key={index}
-                                style={{ width: "18rem", padding: "10px", margin: "20px", borderRadius: "10px", boxShadow: "0 0 10px #999999", display: "flex", flexDirection: "column"}}>
-                                <Card.Img variant="top" src={article.urlToImage} />
+                                style={{ width: "20rem", padding: "10px", margin: "20px", borderRadius: "10px", boxShadow: "0 0 10px #999999", display: "flex", flexBasis: "auto"}}>
+                                {(article.image != null ?
+                                (<Card.Img style={{width: "100%", height: "250px"}} variant="top" src={article.image} />)
+                                : (<Card.Img style={{width: "100%", height: "250px"}} variant="top" src={Noimage} />)
+                                )};
                                 <Card.Body>
                                     <Card.Title>{article.title}</Card.Title>
-                                    <Card.Text>{article.content}</Card.Text>
-                                    <footer className="blockquote-footer">
-                                        Published by <cite title="Source Title">{article.source.name} <br /> {(new Date(article.publishedAt)).toLocaleDateString()}</cite>
-                                    </footer>
-                                    <Button style={{marginTop: "auto"}} variant="primary" target="__blank" href={article.url} style={{ width: "15rem" }}>Read article</Button>   
-                                    <Button  style={{marginTop: "auto"}} variant="success" className="mt-2" value={article.url + " * " + article.title} onClick={this.handleStorage}  style={{ width: "15rem" }}>Save article</Button>   
+                                    {article.description.length > maxlength ?
+                                    (
+                                        <Card.Text>{`${article.description.substring(0, maxlength)}...`}
+                                         <footer className="blockquote-footer mt-5">
+                                        Published by <cite title="Source Title">{article.source} <br /> {(new Date(article.published_at)).toLocaleDateString()}</cite>
+                                        </footer>
+                                        <Button style={{marginTop: "auto"}} variant="primary" target="__blank" href={article.url} style={{ width: "15rem" }}>Read full article</Button>   
+                                        <Button  style={{marginTop: "auto"}} variant="success" className="mt-2" value={article.url + " * " + article.title} onClick={this.handleStorage}  style={{ width: "15rem" }}>Save article</Button>   
+                                        </Card.Text>
+                                    ) :
+                                    
+                                        <Card.Text>{article.description}
+                                         <footer className="blockquote-footer mt-5">
+                                        Published by <cite title="Source Title">{article.source} <br /> {(new Date(article.published_at)).toLocaleDateString()}</cite>
+                                        </footer>
+                                        <Button style={{marginTop: "auto"}} variant="primary" target="__blank" href={article.url} style={{ width: "15rem" }}>Go to the site</Button>   
+                                        <Button  style={{marginTop: "auto"}} variant="success" className="mt-2" value={article.url + " * " + article.title} onClick={this.handleStorage}  style={{ width: "15rem" }}>Save article</Button>   
+
+                                        </Card.Text>
+                                    
+                                }
+                                   
 
 
                                 </Card.Body>
